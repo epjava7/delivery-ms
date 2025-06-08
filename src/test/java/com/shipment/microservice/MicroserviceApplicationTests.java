4368d7d679b8dee424ebb98c55953432e21a3ead
epjava7 <ericpanjava7@gmail.com>
2025-06-07 20:45:32 -0700

    fixed test class stopping build

--- src/test/java/com/shipment/microservice/MicroserviceApplicationTests.java
+++ src/test/java/com/shipment/microservice/MicroserviceApplicationTests.java
@@ -3,7 +3,7 @@ package com.shipment.microservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = com.delivery.ms.DeliveryMsApplication.class)
 class MicroserviceApplicationTests {
 
 	@Test
